e09eed39b45e5c1aa8360c60b2adf2f61ca2bbb0
Carla Rani <carla.rani@gmail.com>
2023-03-27 21:36:04 -0300

    fim da aula

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-let usuarios = []
+let usuarios = [];
+let produtos = [];
 
-function formCadastro() {
+let btnCadastro = document.querySelector("#btn-cadastro").addEventListener("click", adicionarUsuario)
+let btnLogin = document.querySelector("#btn-login").addEventListener("click", validaUsuario)
+let btnCarrinho = document.querySelector("#btn-carrinho").addEventListener("click", adicionarProduto)
 
-    let name = document.querySelector("#exampleInputName").value
-    let email = document.querySelector("#exampleInputEmail").value
-    let password = document.querySelector("#exampleInputPassword").value
 
+function adicionarUsuario() {
     let user = {
-        name,
-        email,
-        password
+        name: document.querySelector("#cadInputName").value,
+        email: document.querySelector("#cadInputEmail").value,
+        password: document.querySelector("#cadInputPassword").value
     }
-
+    // console.log(user)
     usuarios.push(user)
+    alert("Usuário adicionado com sucesso")
     console.log(usuarios);
 }
+
+function validaUsuario(){
+
+    let email = document.querySelector("#loginInputEmail").value;
+    let password= document.querySelector("#loginInputPassword").value;
+    let checkUser = usuarios.find(usuario => usuario.email==email);
+    console.log(checkUser)
+    let result=false;
+    if(!checkUser)
+    {
+        document.querySelector("#return-failed-login").innerHTML = "<p>Usuário não cadastrado.</p>"
+    }else if(checkUser){
+        result = checkUser.password == password
+        let name = checkUser.name;
+        if(result)
+        {
+            $("#ModalLogin").modal('hide');
+            let el = document.querySelector("#header-saudacao");
+            el.setAttribute("class", "saudacao")
+            let text = "Olá, "+name;
+            let elemento= "<p>"+text+"</p>";
+            el.innerHTML= elemento
+        }else{
+            document.querySelector("#return-failed-login").innerHTML = "<p>Senha incorreta.</p>"
+        }
+        console.log(result);
+    }else {
+        alert("Ocorreu um erro, tente novamente mais tarde")
+    }
+}
+
+function adicionarProduto() {
+    $("#ModalCarrinho").modal('show');
+
+    let produto = {
+        quantity: document.querySelector("#input-qty").value,
+        color: document.querySelector("#select-color").value,
+    }
+    console.log(produto)
+    produtos.push(produto)
+    console.log(produtos);
+}
+
+

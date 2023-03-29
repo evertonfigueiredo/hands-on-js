e8bc70bc937ed3f067e4be6ae6f6434c44419f9a
pepekoerich <pedro_koerich@hotmail.com>
2023-03-28 22:41:30 -0300

    pedro-caldeira

--- index.js
+++ index.js
@@ -1,17 +1,97 @@
-let usuarios = []
+class User {
 
-function formCadastro() {
+        constructor(){
+            this.usuarios = []
+        }
 
-    let name = document.querySelector("#exampleInputName").value
-    let email = document.querySelector("#exampleInputEmail").value
-    let password = document.querySelector("#exampleInputPassword").value
+        formCadastro() {
 
-    let user = {
-        name,
-        email,
-        password
+            let name = document.querySelector("#cadInputName")
+            let email = document.querySelector("#cadInputEmail")
+            let password = document.querySelector("#cadInputPassword")
+
+            let user = {
+                name: name.value,
+                email: email.value,
+                password: password.value
+            }
+            let produtoRepetido = this.usuarios.find(item => item.email === email.value)
+
+            if (produtoRepetido != undefined) {
+                alert('Email Já Cadastrado')
+            } else if (name.value != '' && email.value != '' && password.value != '') {
+                this.usuarios.push(user)
+                $('#ModalCadastro').modal('hide');
+            } else {
+                alert('Preencha todos os campos')
+            }
+
+            console.log(this.usuarios);
+            name.value = ''
+            email.value = ''
+            password.value = ''         
+        }
+
+        formLogin() { 
+
+            let email = document.querySelector("#loginInputEmail")
+            let password = document.querySelector("#loginInputPassword")
+
+            this.usuarios.map(usuario =>{
+
+                if (usuario.email === email.value && usuario.password === password.value) {
+                    alert('Login efetuado com sucesso')
+                    $('#ModalLogin').modal('hide');
+                    console.log(usuario.name);
+                } else {
+                    alert('Email ou senha incorretos')
+                }
+                
+            })
+
+           
+        }
+}
+
+let meuUsuario = new User()
+
+class Carrinho {
+
+        constructor(){
+            this.produtos = []
+}
+
+        adicionarProduto(){
+
+            let qty = document.querySelector("#input-qty")
+            let color = document.querySelector('#select-color')
+        
+            let produto = {
+                qty: qty.value,
+                color: color.value,
+                preco: qty.value * 150
+            }
+
+            this.produtos.push(produto)
+            console.log(this.produtos);
+            $('#ModalCarrinho').modal('show');
+
+            this.produtos.map(produto =>{
+                
+                let mostraPreco = document.querySelector("#precoModal")
+                let mostraQty = document.querySelector("#qtyModal")
+                let mostraColor = document.querySelector("#corModal")
+
+                mostraPreco.textContent = 'R$ ' + produto.preco
+                mostraQty.textContent = produto.qty
+                mostraColor.textContent = produto.color
+               
+            }
+        )     
     }
 
-    usuarios.push(user)
-    console.log(usuarios);
+        
 }
+
+
+let meuCarrinho = new Carrinho()

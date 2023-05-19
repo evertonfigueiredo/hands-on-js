e667fa2ddea10333c14cf9eaf75b57b0c4af1c6f
Haydê Carlos <86674944+haydecarlos@users.noreply.github.com>
2023-05-18 21:19:38 -0300

    Funcao para encontrar usuario

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let usuarios = []
 
 function formCadastro() {
 
-    let name = document.querySelector("#exampleInputName").value
-    let email = document.querySelector("#exampleInputEmail").value
-    let password = document.querySelector("#exampleInputPassword").value
+    let name = document.querySelector("#cadInputName").value
+    let email = document.querySelector("#cadInputEmail").value
+    let password = document.querySelector("#cadInputPassword").value
 
     let user = {
-        name: name.value,
-        email: email.value,
-        password: password.value
+        name,
+        email,
+        password
     }
 
     name.value = ''
@@ -18,5 +18,29 @@ function formCadastro() {
 
     usuarios.push(user)
     console.log(usuarios);
+
 }
 
+// 2
+
+function validarCadastro() {
+    let email = document.querySelector("#loginInputEmail").value
+    let password = document.querySelector("#loginInputPassword").value
+    let name = ''
+
+    for (let i = 0; i < usuarios.length; i++) {
+        usuario = usuarios[i]
+        if (usuario.password === password && usuario.email === email) {
+            name = usuario.name
+        }
+    }
+    console.log(name)
+}
+
+validarCadastro()
+
+
+// 
+function adicionarItem() {
+
+}

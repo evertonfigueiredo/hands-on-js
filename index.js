fda2b86fdbbe8bee67827f2609b91dfede5cc291
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-07-18 18:47:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,4 @@ let usuarios = []
 function formCadastro() {
 
     let name = document.querySelector("#exampleInputName").value
-    let email = document.querySelector("#exampleInputEmail").value
-    let password = document.querySelector("#exampleInputPassword").value
-
-    let user = {
-        name,
-        email,
-        password
-    }
-
-    usuarios.push(user)
-    console.log(usuarios);
 }

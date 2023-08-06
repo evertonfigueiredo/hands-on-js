1b80510e2aa0fd0e63f51ef138a953248d66ab54
Matheus B. Gomes <matheusbgomes@hotmail.com>
2023-08-06 00:06:43 -0300

    feat: display cart

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-let usuarios = []
-let cart = []
-const nameCad = document.querySelector("#cadInputName")
-const emailCad = document.querySelector("#cadInputEmail")
-const passwordCad = document.querySelector("#cadInputPassword")
-const submitBtn = document.querySelector("#cadSubmit")
-const loginBtn = document.querySelector("#btnLogin")
-const emailLogin = document.querySelector("#loginInputEmail")
-const passwordLogin = document.querySelector("#loginInputPassword")
-const nameCart = document.querySelector("#itemName")
-const priceCart = document.querySelector("#product-form-price")
-const qtyCart = document.querySelector("#input-qty")
-const colorCart = document.querySelector("#select-color")
 
-
+const usuarios = [];
+const cart = [];
 
 //1 - Crie uma função que pegue os valores do formulário de cadastro e adicione em um array um objeto para ser acesso posteriormente.
 
 function registerUser(){
+    const nameCad = document.querySelector("#cadInputName")
+    const emailCad = document.querySelector("#cadInputEmail")
+    const passwordCad = document.querySelector("#cadInputPassword")
+    const submitBtn = document.querySelector("#cadSubmit")
     let name = nameCad.value
     let email = emailCad.value
     let password = passwordCad.value
@@ -51,6 +43,9 @@ submit.addEventListener("click",registerUser)
 //2 - Com base no tópico anterior crie uma função que acesse o array criado e com o email e senha passado no formulário de login retorne o nome do usuário cadastrado.
 
 function userAuth(){
+    const loginBtn = document.querySelector("#btnLogin")
+    const emailLogin = document.querySelector("#loginInputEmail")
+    const passwordLogin = document.querySelector("#loginInputPassword")
     let email = emailLogin.value
     let password = passwordLogin.value
     let findUser = usuarios.find(user => user.email === email)
@@ -84,6 +79,10 @@ login.addEventListener("click",userAuth)
 //     Obs.: Você deve criar um array para armazenar os dados do produto e logo em seguida mostrar o array na lista do carrinho.
 
 function addItem(){
+    const nameCart = document.querySelector("#itemName")
+    const priceCart = document.querySelector("#product-form-price")
+    const qtyCart = document.querySelector("#input-qty")
+    const colorCart = document.querySelector("#select-color")
     let itemName = nameCart.textContent
     let itemPrice = priceCart.textContent
     let itemQuantity = Number(qtyCart.value)
@@ -100,8 +99,61 @@ function addItem(){
             }
             cart.push(item)
     }
-    console.log(cart)
 }
 
 let addCart = document.querySelector("#addCart")
 addCart.addEventListener("click",addItem)
+
+function showCartContent(){
+        const cartContent = document.querySelector("#cart-content")
+        cartContent.innerHTML = '';
+        cart.forEach((item) => {
+            const card = document.createElement('div');
+            card.classList.add('card-body');
+
+            const row = document.createElement('div');
+            row.classList.add('row', 'row-cart');
+
+            const col = document.createElement('div');
+            col.classList.add('col');
+
+            const itemName = document.createElement('h5');
+            itemName.classList.add('card-title');
+            itemName.textContent = item.itemName
+
+            const itemPriceText = document.createElement('p')
+            itemPriceText.classList.add('card-text');
+            itemPriceText.textContent = 'Preço: '
+
+            const itemPrice = document.createElement('span')
+            itemPrice.textContent = item.itemPrice
+
+            const itemQtyText = document.createElement('p')
+            itemQtyText.classList.add('card-text');
+            itemQtyText.textContent = 'Quantidade: '
+
+            const itemQty = document.createElement('span')
+            itemQty.textContent = item.itemQuantity
+
+            const itemColorText = document.createElement('p')
+            itemColorText.classList.add('card-text');
+            itemColorText.textContent = 'Cor: '
+
+            const itemColor = document.createElement('span')
+            itemColor.textContent = item.itemColor
+
+            row.appendChild(itemName)
+            row.appendChild(itemPriceText)
+            itemPriceText.appendChild(itemPrice)
+            row.appendChild(itemQtyText)
+            itemQtyText.appendChild(itemQty)
+            row.appendChild(itemColorText)
+            itemColorText.appendChild(itemColor)
+
+            card.appendChild(row)
+            cartContent.appendChild(card);        
+        })
+}
+
+let showCart = document.querySelector("#show-cart")
+showCart.addEventListener("click", showCartContent)
